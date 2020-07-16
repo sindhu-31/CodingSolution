1adb6a22c1a55bf4c3db47f174a07f034d14b18a
Sindhuja Ramesh <sindhu.sassy31@gmail.com>
2020-07-16 07:52:05 +0530

    Committing for Bonus Q2 -- Not completed

---
+++ src/main/java/Application.java
@@ -0,0 +1,7 @@
+package main.java;
+@SpringBootApplication
+public class Application {
+	public static void main(String... args) {
+		SpringApplication.run(Application.class, args);
+	}
+}
---
+++ src/main/java/MyRestController.java
@@ -0,0 +1,8 @@
+package main.java;
+
+
+
+@RestController
+public class MyRestController {
+
+}
